0ea09064c63c639838049d8c0747a86097f49b97
ibrahim <ibrahiminfo8@gmail.com>
2025-05-16 22:15:15 +0530

    completed shadcn installation and alias import

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<><h1 className="text-amber-200">Home</h1></>
+    }
+])
+
+export default router
